5f83968781712fd806b89fbaabd65581732aad50
billkiller <747441355@qq.com>
2018-04-29 20:36:20 +0800

    修复了长方形bug和新增了注释

--- src/model/MyRectangle.java
+++ src/model/MyRectangle.java
@@ -28,12 +28,17 @@ public class MyRectangle extends  MyShape {
     }
     public void setWidth(double width){
         this.width=width;
+        this.x = leftX+width;
+    	this.y = leftY+height;
         rectangle.setWidth(2*width);
         updateRectangle();
     }
     public  void setHeight(double height){
     	this.height=height;
+    	this.x = leftX+width;
+    	this.y = leftY+height;
     	rectangle.setHeight(2*height);
+        updateRectangle();
     }
     @Override
     public void Move(double x,double y){
